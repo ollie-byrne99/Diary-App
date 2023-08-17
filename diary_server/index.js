49892d323fb89295b9e1252ca9f581ffce431a7a
Stephanie <stephf202@gmail.com>
2023-08-17 16:07:22 +0100

    add index.html and update setup.sql

--- diary_server/index.js
+++ diary_server/index.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 
 const app = require("./app");
+const port = process.env.PORT || 3000
 
-app.listen(process.env.PORT, () => {
-    console.log(`API listening on ${process.env.PORT}`);
+app.listen(port, () => {
+    console.log(`API listening on ${port}`);
 })
 
