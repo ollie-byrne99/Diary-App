31496c8a4850bd32883e42165600cbf142ee4f4e
Ollie <olliebyrne2@outlook.com>
2023-08-17 16:03:58 +0100

    fixed SQL typo and got server running

--- diary_server/database/setup.sql
+++ diary_server/database/setup.sql
@@ -4,13 +4,13 @@ CREATE TABLE diaries (
   diary_id INT GENERATED ALWAYS AS IDENTITY,
   diary_name VARCHAR(50) UNIQUE,
   diary_text VARCHAR(500),
-  category VARCHAR(30)
+  category VARCHAR(30),
   PRIMARY KEY (diary_id)
-)
+);
 
 INSERT INTO diaries
   (diary_name, diary_text, category)
 VALUES
   ('diary 1', 'diary text1', 'wildest dreams'), 
   ('diary 2', 'diary text2', 'deepest fear'),
-  ('diary 3', 'diary text3', 'regrettable choices')
+  ('diary 3', 'diary text3', 'regrettable choices');
