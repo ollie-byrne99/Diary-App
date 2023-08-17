a8f4350fbac7d5ecaf628642988440d6bc69d197
Stephanie <stephf202@gmail.com>
2023-08-17 15:09:06 +0100

    update setup.sql

--- diary_server/database/setup.sql
+++ diary_server/database/setup.sql
@@ -2,10 +2,15 @@ DROP TABLE IF EXISTS diaries;
 
 CREATE TABLE diaries (
   diary_id INT GENERATED ALWAYS AS IDENTITY,
-  diary_name VARCHAR(50)
+  diary_name VARCHAR(50) UNIQUE,
+  diary_text VARCHAR(500),
+  category VARCHAR(30)
+  PRIMARY KEY (diary_id)
 )
 
 INSERT INTO diaries
-  (diary_name)
+  (diary_name, diary_text, category)
 VALUES
-  ('diary 1')
+  ('diary 1', 'diary text1', 'wildest dreams'), 
+  ('diary 2', 'diary text2', 'deepest fear'),
+  ('diary 3', 'diary text3', 'regrettable choices')
